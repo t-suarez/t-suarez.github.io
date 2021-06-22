08d83d25e6a8a6842af594658bc3adaeecec4c8c
Ubuntu <ubuntu@ip-172-31-68-44.ec2.internal>
2021-06-22 05:38:41 +0000

    add portfolio.html file

---
+++ projects/scratch-pad/day-4/contact-list.js
@@ -0,0 +1,111 @@
+// #!/usr/bin/env node
+
+'use strict';
+
+/**
+ * 4: Contact List
+ * 
+ *  a. Create a factory Function called makeContact(id, nameFirst, nameLast) 
+ *     that returns a contact object.
+ *     
+ *          ex: makeContact(1, 'Max', 'Gaudin'); // => {id: 1, nameFirst: 'Max', nameLast: 'Gaudin'}
+ *     
+ *  b. Create a factory Function called makeContactList that returns an Object 
+ *     that manages contacts. The contact-list object should have the following methods:
+ *       
+ *      1. length(): returns the number of contacts within the list.
+ *      2. addContact(contact): takes a contact object to be added to the 
+ *         contact-list.
+ *      3. findContact(fullName): takes a full-name String, like 'Max Gaudin', and 
+ *         returns the contact object if found in the contacts-list, or, 
+ *         undefined if the fullName does not match any contacts in the list.
+ *      4. removeContact(contact): takes a contact object to be removed from 
+ *         the contact-list.
+ *      5. add a printAllContactNames() Function to your makeContactList() factory. The printAllContactNames() Function should 
+ *         return a String formated with all the full-names of the separated 
+ *         with a line-break, like so:
+ *          
+ *         myContacts.printAllContactNames(); // => Max Gaudin
+ *                                                  John Fraboni
+ *                                                  Kaelyn Chresfield
+ *          
+ *          WARNING: To pass this test, the LAST full name should have NO
+ *          new-line character added after it!
+ */
+
+// YOUR CODE GOES BELOW HERE //
+function makeContact(id, nameFirst, nameLast) {
+    var obj = {
+        id: id,
+        nameFirst: nameFirst,
+        nameLast: nameLast
+    }
+    return obj;
+} 
+
+
+function makeContactList() {
+    /*
+     * You need something here to hold contacts. See length api for a hint:
+     */
+    var contacts = [];
+    
+    return {
+        // we implemented the length api for you //
+        length: function() {
+            return contacts.length;
+        },
+        
+        addContact: function(contact){
+            contacts.push(contact);
+        },
+        
+        findContact: function(fullName){
+            var splitName = fullName.split(" ");
+            for(var i = 0; i < contacts.length; i++){
+                if(contacts[i].nameFirst === splitName[0] && contacts[i].nameLast === splitName[1]){
+                    return contacts[i];
+                }
+            }
+            return undefined;
+        },
+        
+        removeContact: function(contact){
+            for(var i = 0; i < contacts.length; i++){
+                if(contacts[i] === contact){
+                    contacts.splice(contacts[i], 1);
+                }
+            }
+        },
+        
+        printAllContactNames: function(){
+            var nameArray = [];
+            for(var i = 0; i < contacts.length; i++){
+                if(i === contacts.length - 1){
+                    var fullName = contacts[i].nameFirst + " " + contacts[i].nameLast;
+                }
+                else{
+                    var fullName = contacts[i].nameFirst + " " + contacts[i].nameLast + "\n";
+                }
+                nameArray[i] = fullName;
+            }
+            return nameArray.join("");
+        }
+    };
+}
+
+
+
+
+// YOUR CODE GOES ABOVE HERE //
+
+
+
+
+// DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
+if((typeof process !== 'undefined') &&
+(typeof process.versions.node !== 'undefined')) {
+    // here, export any references you need for tests //
+    module.exports.makeContact = makeContact;
+    module.exports.makeContactList = makeContactList;
+}
